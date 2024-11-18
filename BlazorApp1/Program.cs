8391c371f5c68c3b39301d4adc8070e6db06d424
Inckrisz <inckrisz28@gmail.com>
2024-11-18 20:23:29 +0100

    new webasm app

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace AutoWorkshopClient
+namespace BlazorApp1
 {
     public class Program
     {
